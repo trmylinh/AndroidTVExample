f29aada1a7b273b1d0a70f519c93afc619269a5e
LinhTM_Android <linhtm@infinitytech.global>
2025-03-20 17:50:24 +0700

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.example.androidtvexample"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.androidtvexample"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.leanback)
+    implementation(libs.glide)
+}
---
+++ app/src/main/java/com/example/androidtvexample/MainFragment.kt
@@ -0,0 +1,236 @@
+package com.example.androidtvexample
+
+import java.util.Collections
+import java.util.Timer
+import java.util.TimerTask
+
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.leanback.app.BackgroundManager
+import androidx.leanback.app.BrowseSupportFragment
+import androidx.leanback.widget.ArrayObjectAdapter
+import androidx.leanback.widget.HeaderItem
+import androidx.leanback.widget.ImageCardView
+import androidx.leanback.widget.ListRow
+import androidx.leanback.widget.ListRowPresenter
+import androidx.leanback.widget.OnItemViewClickedListener
+import androidx.leanback.widget.OnItemViewSelectedListener
+import androidx.leanback.widget.Presenter
+import androidx.leanback.widget.Row
+import androidx.leanback.widget.RowPresenter
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.content.ContextCompat
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
+
+/**
+ * Loads a grid of cards with movies to browse.
+ */
+class MainFragment : BrowseSupportFragment() {
+
+    private val mHandler = Handler(Looper.myLooper()!!)
+    private lateinit var mBackgroundManager: BackgroundManager
+    private var mDefaultBackground: Drawable? = null
+    private lateinit var mMetrics: DisplayMetrics
+    private var mBackgroundTimer: Timer? = null
+    private var mBackgroundUri: String? = null
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        Log.i(TAG, "onCreate")
+        super.onActivityCreated(savedInstanceState)
+
+        prepareBackgroundManager()
+
+        setupUIElements()
+
+        loadRows()
+
+        setupEventListeners()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: " + mBackgroundTimer?.toString())
+        mBackgroundTimer?.cancel()
+    }
+
+    private fun prepareBackgroundManager() {
+        mBackgroundManager = BackgroundManager.getInstance(activity)
+        mBackgroundManager.attach(activity!!.window)
+        mDefaultBackground = ContextCompat.getDrawable(context!!, R.drawable.default_background)
+        mMetrics = DisplayMetrics()
+        activity!!.windowManager.defaultDisplay.getMetrics(mMetrics)
+    }
+
+    private fun setupUIElements() {
+        title = getString(R.string.browse_title)
+//        badgeDrawable = resources.getDrawable(R.mipmap.ic_launcher, null)
+        // over title
+        headersState = BrowseSupportFragment.HEADERS_ENABLED
+        isHeadersTransitionOnBackEnabled = true
+
+        // set fastLane (or headers) background color
+        brandColor = ContextCompat.getColor(context!!, R.color.fastlane_background)
+        // set search icon color
+        searchAffordanceColor = ContextCompat.getColor(context!!, R.color.search_opaque)
+    }
+
+    private fun loadRows() {
+        val rowsAdapter = ArrayObjectAdapter(ListRowPresenter())
+
+        /* GridItemPresenter */
+        val gridHeader = HeaderItem(0, "GridItemPresenter")
+
+        val mGridPresenter = GridItemPresenter()
+        val gridRowAdapter = ArrayObjectAdapter(mGridPresenter)
+        gridRowAdapter.add("CardInfo 1")
+        gridRowAdapter.add("CardInfo 2")
+        gridRowAdapter.add("CardInfo 3")
+        gridRowAdapter.add("CardInfo 4")
+        rowsAdapter.add(ListRow(gridHeader, gridRowAdapter))
+
+        /* CardPresenter */
+        val cardPresenterHeader = HeaderItem(0, "CardPresenter")
+        val cardPresenter = CardPresenter()
+        val cardRowAdapter = ArrayObjectAdapter(cardPresenter)
+
+        for (i in 0 until NUM_ROWS) {
+            val movie = Movie()
+            movie.title = "Title $i"
+            movie.studio = "Studio $i"
+            movie.cardImageUrl = "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review/card.jpg"
+            cardRowAdapter.add(movie)
+        }
+        rowsAdapter.add(ListRow(cardPresenterHeader, cardRowAdapter))
+
+        /*set*/
+        adapter = rowsAdapter
+    }
+
+    private fun setupEventListeners() {
+        setOnSearchClickedListener {
+            Toast.makeText(context!!, "Implement your own in-app search", Toast.LENGTH_LONG)
+                .show()
+        }
+
+        onItemViewClickedListener = ItemViewClickedListener()
+        onItemViewSelectedListener = ItemViewSelectedListener()
+    }
+
+    private inner class ItemViewClickedListener : OnItemViewClickedListener {
+        override fun onItemClicked(
+            itemViewHolder: Presenter.ViewHolder,
+            item: Any,
+            rowViewHolder: RowPresenter.ViewHolder,
+            row: Row
+        ) {
+
+            if (item is Movie) {
+                Log.d(TAG, "Item: $item")
+                val intent = Intent(context!!, DetailsActivity::class.java)
+                intent.putExtra(DetailsActivity.MOVIE, item)
+
+                val bundle = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                    activity!!,
+                    (itemViewHolder.view as ImageCardView).mainImageView,
+                    DetailsActivity.SHARED_ELEMENT_NAME
+                )
+                    .toBundle()
+                startActivity(intent, bundle)
+            } else if (item is String) {
+                if (item.contains(getString(R.string.error_fragment))) {
+                    val intent = Intent(context!!, BrowseErrorActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(context!!, item, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private inner class ItemViewSelectedListener : OnItemViewSelectedListener {
+        override fun onItemSelected(
+            itemViewHolder: Presenter.ViewHolder?, item: Any?,
+            rowViewHolder: RowPresenter.ViewHolder, row: Row
+        ) {
+            if (item is Movie) {
+//                mBackgroundUri = item.backgroundImageUrl
+                mBackgroundUri = item.cardImageUrl
+                startBackgroundTimer()
+            }
+        }
+    }
+
+    private fun updateBackground(uri: String?) {
+        val width = mMetrics.widthPixels
+        val height = mMetrics.heightPixels
+        Glide.with(context!!)
+            .load(uri)
+            .centerCrop()
+            .error(mDefaultBackground)
+            .into<SimpleTarget<Drawable>>(
+                object : SimpleTarget<Drawable>(width, height) {
+                    override fun onResourceReady(
+                        drawable: Drawable,
+                        transition: Transition<in Drawable>?
+                    ) {
+                        mBackgroundManager.drawable = drawable
+                    }
+                })
+        mBackgroundTimer?.cancel()
+    }
+
+    private fun startBackgroundTimer() {
+        mBackgroundTimer?.cancel()
+        mBackgroundTimer = Timer()
+        mBackgroundTimer?.schedule(UpdateBackgroundTask(), BACKGROUND_UPDATE_DELAY.toLong())
+    }
+
+    private inner class UpdateBackgroundTask : TimerTask() {
+
+        override fun run() {
+            mHandler.post { updateBackground(mBackgroundUri) }
+        }
+    }
+
+    private inner class GridItemPresenter : Presenter() {
+        override fun onCreateViewHolder(parent: ViewGroup): Presenter.ViewHolder {
+            val view = TextView(parent.context)
+            view.layoutParams = ViewGroup.LayoutParams(GRID_ITEM_WIDTH, GRID_ITEM_HEIGHT)
+            view.isFocusable = true
+            view.isFocusableInTouchMode = true
+            view.setBackgroundColor(ContextCompat.getColor(context!!, R.color.default_background))
+            view.setTextColor(Color.WHITE)
+            view.gravity = Gravity.CENTER
+            return Presenter.ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(viewHolder: Presenter.ViewHolder, item: Any) {
+            (viewHolder.view as TextView).text = item as String
+        }
+
+        override fun onUnbindViewHolder(viewHolder: Presenter.ViewHolder) {}
+    }
+
+    companion object {
+        private val TAG = "MainFragment"
+
+        private val BACKGROUND_UPDATE_DELAY = 300
+        private val GRID_ITEM_WIDTH = 200
+        private val GRID_ITEM_HEIGHT = 200
+        private val NUM_ROWS = 6
+        private val NUM_COLS = 15
+    }
+}
